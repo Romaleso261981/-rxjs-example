a827dfcb83d1162adfc6946fe6b16f0f9f6e015f
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-12-12 16:00:53 +0300

    fix

---
+++ src/creation.js
@@ -0,0 +1,12 @@
+import {Observable} from 'rxjs'
+/*
+import { from } from 'rxjs/src';
+const stream$ = of(1,2,3,6,5,4,7)
+stream$.subscribe(value => {
+  console.log(value);
+})
+
+const arr& = from([1,2,3,6,5,4,8,8]).pipe(
+  scan((acc, v)=>acc.concat(v), [])
+)
+*/
